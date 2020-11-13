11b4d75059af235caf840e45086bca02ec58dc11
Tomer Michael <tmichael@lightricks.com>
2020-11-13 14:49:25 +0200

    Move text_recognition package.

--- app/src/main/java/com/billy/billy/text_recognition/BillParser.java
+++ app/src/main/java/com/billy/billy/text_recognition/BillParser.java
@@ -1,10 +1,10 @@
-package com.billy.text_recognition;
+package com.billy.billy.text_recognition;
 
 import android.content.Context;
 
-import androidx.annotation.NonNull;
-
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
+
+import androidx.annotation.NonNull;
 
 public interface BillParser {
     @NonNull Bill parse(@NonNull Context context, @NonNull FirebaseVisionText firebaseVisionText);
