d3c5ae70d163469b798b54c3b85a2b1eeb828d25
Tomer Michael <tmichael@lightricks.com>
2020-11-29 21:51:16 +0200

    Small tweaks in HistoryFragment.

--- app/src/main/java/com/billy/billy/bill/HistoryFragment.java
+++ app/src/main/java/com/billy/billy/bill/HistoryFragment.java
@@ -5,24 +5,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.billy.billy.R;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.billy.billy.R;
-
 public class HistoryFragment extends Fragment {
     private HistoryViewModel historyViewModel;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        historyViewModel = (new ViewModelProvider(this)).get(HistoryViewModel.class);
+    }
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        historyViewModel = (new ViewModelProvider(this)).get(HistoryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_history, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        historyViewModel.getText().observe(getViewLifecycleOwner(), s -> textView.setText(s));
-        return root;
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_history, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final TextView textView = view.findViewById(R.id.text_dashboard);
+        textView.setOnClickListener(v ->
+                Toast.makeText(requireContext(), R.string.history_coming_soon_title, Toast.LENGTH_LONG).show());
+
+        historyViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
     }
 }
